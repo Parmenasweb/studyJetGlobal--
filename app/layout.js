737b669909ab74096f04a056c3a9ab9fdc3c67fb
Parmenasweb <parmabusiness10@gmail.com>
2024-07-21 09:34:03 +0530

    light mode activated

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default async function RootLayout({ children }) {
         <AuthProvider session={session}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
